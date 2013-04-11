29a462eba13dbe0cd5e9894e2d0f416e5893dba3
Martin M B <bjrmar10@nith.no>
2013-04-11 17:42:51 +0200

    level.cs
    added a lv class with spawn points and the like.

---
+++ Shadows/Shadows/Level.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Shadows
+{
+    class Level
+    {
+        Texture2D map { get; protected set; }
+        Texture2D miniMap { get; protected set; }
+        Vector2 playerSpawn { get; protected set; }
+        List<Vector2> zombieSpawns = new List<Vector2>();
+
+
+        public Level (Texture2D map, Texture2D miniMap, Vector2 playerSpawn){
+            this.map = map;
+            this.miniMap = miniMap;
+            this.playerSpawn = playerSpawn;
+        }
+
+        public void addZombie(Vector2[] array)
+        {
+            foreach (Vector2 pos in array)
+            {
+                zombieSpawns.Add(pos);
+            }
+        }
+    
+     }
+    
+
+
+}
